f9597dc0473525e259b92cf2697cfb24da95b1ac
vanskins <vansabacajan@gmail.com>
2025-02-04 13:52:50 +0800

    Update: improve ui design

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -9,22 +9,21 @@ interface CardI {
 export const Card = ({ item }: CardI) => {
   console.log(item)
   return (
-    <div className="flex flex-col bg-slate-200 w-full max-w-md rounded-lg shadow-md">
-
-      <div className="w-full">
+    <div className="flex flex-col bg-white w-full max-w-md rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
+      <div className="w-full rounded-t-lg overflow-hidden">
         <Image
           width={400}
           height={400}
           src={item.image}
           alt={item.advertiser}
-          className="w-full h-auto object-cover rounded-t-lg"
+          className="w-full h-auto object-cover"
         />
       </div>
-      <div className="p-4">
-        <p>ID - {item.id}</p>
-        <p>by - {item.advertiser}</p>
-        <p>{item.address}</p>
-        <p>{item.billboardText}</p>
+      <div className="p-6">
+        <h2 className="text-xl font-semibold text-gray-800 mb-2">{item.advertiser}</h2>
+        <p className="text-sm text-gray-500 mb-2">ID: {item.id}</p>
+        <p className="text-sm text-gray-600 mb-4">{item.address}</p>
+        <p className="text-sm text-gray-700">{item.billboardText}</p>
       </div>
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,9 @@ export default function Home() {
   const [billboardData, setBillboardData] = useState<billboardTypes[]>()
   const [instructions, setInstructions] = useState<string | null>()
   const [error, setError] = useState<string | null>()
+  const [clipboardMessage, setClipboardMessage] = useState<string>("")
 
+  const sampleInput = ["x^xv", "x^^x>>xvvx<<x", "xx"]
   const onSubmit = () => {
     if (instructions) {
       postInstructions(instructions).then((result: postInstructionsTypes) => {
@@ -35,31 +37,53 @@ export default function Home() {
     }
 
   }
+
+  const copyToClipBoard = (e: React.MouseEvent) => {
+    const textToCopy = (e.target as HTMLElement).innerText;
+    navigator.clipboard.writeText(textToCopy).then(
+      () => {
+        setClipboardMessage('Text copied to clipboard!');
+      },
+      () => {
+        setClipboardMessage('Failed to copy text.');
+      }
+    );
+    setTimeout(() => setClipboardMessage(""), 1500)
+  }
   return (
-    <div className="container-fluid min-h-screen flex flex-col items-center bg-green-400">
-      <h1 className="text-4xl font-extrabold">DRONE APP</h1>
-      <p>sample x^xv</p>
-      <div className="m-4">
-        <form onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
+    <div className="container-fluid min-h-screen flex flex-col items-center">
+      <h1 className="text-6xl mt-12 font-extrabold">DRONE APP</h1>
+      <div className="text-left mt-4 p-4">
+        <p><span className="font-semibold">Instruction:</span> {`Bigdatr's aerial drone follows simple commands to move and take photos. It can move 1 km in four directions: north (^), south (v), east (>), or west (<). The drone captures a photo when it receives the "x" command.`}</p>
+        <p className="pt-2 font-semibold">Sample input (click to copy to clipboard):
+          {sampleInput.map((i, k) => <span onClick={copyToClipBoard} key={k} className="bg-gray-200 p-2 mx-1 rounded-md cursor-pointer">{i}</span>)}
+        </p>
+        <p className="mt-4">{clipboardMessage}</p>
+      </div>
+      <div className="m-4 lg:w-1/2 w-full p-2 flex">
+        <form className="w-full" onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
           event.preventDefault()
           onSubmit()
         }}>
-          <input
-            onChange={handleOnChange}
-            placeholder="Drone instructions"
-            className="border p-2"
-          />
-          <button
-            className={`p-2 text-white font-semibold ${!!error || !instructions ? "bg-slate-300" : "bg-slate-800"}`}
-            onClick={() => onSubmit()}
-            disabled={!!error || !instructions}
-          >
-            submit
-          </button>
+          <div className="flex">
+            <input
+              onChange={handleOnChange}
+              placeholder="Drone instructions"
+              className="border-2 p-2 w-full border-r-0"
+            />
+            <button
+              className={`w-36 p-2 text-white font-semibold ${!!error || !instructions ? "bg-slate-300" : "bg-slate-800"}`}
+              onClick={() => onSubmit()}
+              disabled={!!error || !instructions}
+            >
+              Submit
+            </button>
+          </div>
+          {error ? <p className="text-red-600 font-semibold">{error}</p> : <p className="mt-6" />}
         </form>
-        {error && <p className="text-red-600 font-semibold">{error}</p>}
       </div>
-      <div className="flex flex-col gap-4 p-4">
+
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-2 p-6">
         {
           billboardData && billboardData.map((item: billboardTypes, key: number) => <Card item={item} key={key} />)
         }
